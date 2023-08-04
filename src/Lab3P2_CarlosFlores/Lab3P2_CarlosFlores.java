50c674172c951e651b1d73900a525a2b1bf46aee
Carlos Flores <carlosfernandoflores321@gmail.com>
2023-08-04 13:35:15 -0600

    segundo commit

--- src/Lab3P2_CarlosFlores/Lab3P2_CarlosFlores.java
+++ src/Lab3P2_CarlosFlores/Lab3P2_CarlosFlores.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication72;
+package Lab3P2_CarlosFlores;
 
 /**
  *
  * @author carlo
  */
-public class JavaApplication72 {
+public class Lab3P2_CarlosFlores {
 
     /**
      * @param args the command line arguments
